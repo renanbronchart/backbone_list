46a1d434a6750e6c8f2c907f14213a59bf682752
Renan Bronchart <renan.bronchart@gmail.com>
2016-12-07 11:42:27 +0100

    [Frontend] Create view Backbone and listener collection in this.

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 });
 
+app.use(function (req, res, next) {
+  res.redirect('/');
+  next();
+});
+
+app.use(function (req, res, next) {
+  res.status(404);
+  res.send('Erreur 404');
+})
+
 app.listen(8000);
